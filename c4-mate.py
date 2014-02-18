9061ed66924eaea9ec0b78e23872e0dcdf6f6b02
Gordin <9ordin@gmail.com>
2014-02-18 22:48:21 +0100

    Added transfer option

--- c4-mate.py
+++ c4-mate.py
@@ -5,53 +5,74 @@ import urllib.request
 from urllib.parse import quote
 import re
 
+me = ""
+
 parser = argparse.ArgumentParser(description=
                                  'Add or subtract money from scratchcard.')
 parser.add_argument('amount', type=float, nargs='*',
                     help='Amount to add or subtract')
+parser.add_argument('-u', '--user', default=me,
+                    help='Account to use. Defaults to "user" variable')
+parser.add_argument('-t', '--to', required=False,
+                    help='Account to transfer money to')
 
-user = ""
-lichturl = "http://licht:8000/mate/" + quote(user)
 args = parser.parse_args()
 
 
-def parse_mate(html):
+def parse_mate(html, user=me):
     return re.match('.*' + user + ': (.*?)&', html, flags=re.DOTALL).group(1)
 
 
-def perform_request(amount):
+def perform_request(amount, user=me):
     if amount > 0:
-        action_strings = {'url': 'add', 'text': 'Added'}
+        action = 'add'
     else:
-        action_strings = {'url': 'distract', 'text': 'Subtracted'}
+        action = 'distract'
         amount = abs(amount)
 
-    url = lichturl + "/" + action_strings['url'] + "/?amount={}".format(amount)
+    lichturl = "http://licht:8000/mate/" + quote(user)
+    url = lichturl + "/" + action + "/?amount={}".format(amount)
 
     f = urllib.request.urlopen(url)
     if amount == 0:
-        log_string = "Checking available money"
+        log_string = "Checking available money of {}".format(user)
+    elif action == 'add':
+        log_string = 'Added {}€ to {}'.format(amount, user)
     else:
-        log_string = action_strings['text'] + " {}€".format(amount)
+        log_string = 'Subtracted {}€ from {}'.format(amount, user)
 
     print(log_string)
     return f.read().decode()
 
 
-def check():
+def check(user=me):
     response = perform_request(0)
     new_amount = parse_mate(response)
-    print("Current Amount is {}€".format(new_amount))
+    print("{} has {}€".format(user, new_amount))
     return float(new_amount)
+
+
+def transfer(user, to, amount):
+    if amount <= 0:
+        print("Transfer more that 0€ plz")
+    elif check(user) < amount:
+        print("{} doesn't have enough money to do this!".format(user))
+    else:
+        perform_request(amount, to)
+        perform_request(-amount, user)
 
 
 if not len(args.amount):
     check()
 
-for amount in args.amount:
-    if amount > 0 or check() + amount >= 0:
-        response = perform_request(amount)
-        new_amount = parse_mate(response)
-        print("New Amount is {}€".format(new_amount))
-    else:
-        print("You don't have enough money to do this!")
+if args.to:
+    for amount in args.amount:
+        transfer(args.user, args.to, amount)
+else:
+    for amount in args.amount:
+        if amount > 0 or check() + amount >= 0:
+            response = perform_request(amount)
+            new_amount = parse_mate(response)
+            print("New Amount is {}€".format(new_amount))
+        else:
+            print("You don't have enough money to do this!")
